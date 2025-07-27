e0e483291609b12718fb47c4752c42fbb8ebe024
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 11:53:21 +0530

    Update ads-728x90.js

--- ads-728x90.js
+++ ads-728x90.js
@@ -1,14 +1,28 @@
 (function() {
-  var atOptions = {
-    'key' : 'b5c9831c055ab6694d6882aa18538653',
-    'format' : 'iframe',
-    'height' : 90,
-    'width' : 728,
-    'params' : {}
-  };
-  var s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.async = true;
-  s.src = 'https://www.highperformanceformat.com/b5c9831c055ab6694d6882aa18538653/invoke.js';
-  document.currentScript.parentNode.insertBefore(s, document.currentScript);
+  var container = document.getElementById("ad-728x90");
+
+  if (container) {
+    // Set Adsterra configuration
+    var configScript = document.createElement('script');
+    configScript.type = 'text/javascript';
+    configScript.innerHTML = `
+      atOptions = {
+        'key' : 'b5c9831c055ab6694d6882aa18538653',
+        'format' : 'iframe',
+        'height' : 90,
+        'width' : 728,
+        'params' : {}
+      };
+    `;
+    container.appendChild(configScript);
+
+    // Load the invoke.js script
+    var adScript = document.createElement('script');
+    adScript.type = 'text/javascript';
+    adScript.src = 'https://www.highperformanceformat.com/b5c9831c055ab6694d6882aa18538653/invoke.js';
+    adScript.async = true;
+    container.appendChild(adScript);
+  } else {
+    console.warn('Ad container #ad-728x90 not found.');
+  }
 })();
