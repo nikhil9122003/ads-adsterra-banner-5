84e6f8b2c8cab87097d3ca41ba5306e5ce2ecf9b
nikhil9122003 <166285467+nikhil9122003@users.noreply.github.com>
2025-07-27 11:31:53 +0530

    Create ads-728x90.js

---
+++ ads-728x90.js
@@ -0,0 +1,14 @@
+(function() {
+  var atOptions = {
+    'key' : 'b5c9831c055ab6694d6882aa18538653',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+  };
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = 'https://www.highperformanceformat.com/b5c9831c055ab6694d6882aa18538653/invoke.js';
+  document.currentScript.parentNode.insertBefore(s, document.currentScript);
+})();
